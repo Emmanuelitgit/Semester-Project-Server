e9db9223997d908e10b1c0c3d835ab7fdc89afae
Emmanuel Yidana <100804353+Emmanuelitgit@users.noreply.github.com>
2023-12-15 09:46:08 +0000

    second commit

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-const database = process.env.DATABASE;
+const database = process.env.DB_NAME;
 
 const db = mysql.createConnection({
     host:dbHost,
