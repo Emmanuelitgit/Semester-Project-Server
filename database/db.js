e8146db63589434e77c354c950f50b1d51bc0421
Emmanuel Yidana <100804353+Emmanuelitgit@users.noreply.github.com>
2023-12-20 14:04:41 +0000

    database

--- database/db.js
+++ database/db.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const dbHost = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
+const dbUser = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
-const database = process.env.DB_NAME;
+const database = process.env.DB_DBNAME;
 
 const db = mysql.createConnection({
     host:dbHost,
@@ -11,5 +12,13 @@ const db = mysql.createConnection({
     password:dbPassword,
     database:database,
 });
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+    console.log('Connected to MySQL!');
+  });
 
 module.exports = db;
