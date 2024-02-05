936ee72676b1081819484e1ce3820ea91fa74927
Emmanuel Yidana <100804353+Emmanuelitgit@users.noreply.github.com>
2024-02-05 23:02:09 +0000

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -74,24 +74,38 @@ const register = (req, res) => {
 };
 
 
-const login = (req, res)=>{
-    const query = "SELECT * FROM users WHERE email = ?";
-    db.query(query, [req.body.email], (err, data)=>{
-        if(err) return res.json(err)
-        if(data.length === 0) return res.status(404).json("User not found!")
+const login = (req, res) => {
+  const query = "SELECT * FROM users WHERE email = ?";
+  db.query(query, [req.body.email], (err, data) => {
+      if (err) {
+          console.error('Database query error:', err);
+          return res.status(500).json(err);
+      }
+      
+      if (data.length === 0) {
+          return res.status(404).json("User not found!");
+      }
 
-        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+      const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
 
-        if(!isPasswordCorrect) return res.status(400).json("Wrong username or password!")
+      if (!isPasswordCorrect) {
+          return res.status(400).json("Wrong username or password!");
+      }
 
-        const token = jwt.sign({id:data[0].id}, "jwtkey");
-        const {password, ...other} = data[0]
+      try {
+          const token = jwt.sign({ id: data[0].id }, "jwtkey");
+          const { password, ...other } = data[0];
 
-        res.cookie("access_token", token, {
-            httpOnly:true
-        }).status(200).json(other)
-    })
-}
+          res.cookie("access_token", token, {
+              httpOnly: true
+          }).status(200).json(other);
+      } catch (jwtError) {
+          console.error('JWT token creation error:', jwtError);
+          return res.status(500).json(jwtError);
+      }
+  });
+};
+
 
 
 const otpStorage = new Map();
